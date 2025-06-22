fb4f9dae37d0ebfa4c36fcf2772d252fc98e5641
nick withers <nickwithers26@gmail.com>
2025-06-22 15:37:23 -0700

    about us content patch

--- src/app/page.js
+++ src/app/page.js
@@ -198,10 +198,12 @@ export default function Home() {
 
                     {/* Content */}
                     <div className="bg-white text-black p-8 text-sm md:text-lg border-l-4 border-[#ca2976] shadow-md leading-relaxed md:leading-loose">
-                        We are a semiconductor consulting firm with deep roots in materials science, process engineering, and technology development. 
-                        With over 30 years of experience across capital equipment OEMs, device manufacturers, and consumable suppliers, we understand how all elements of the ecosystem interact—and why that matters. 
-                        Our team brings a unique blend of technical depth and business insight, supporting clients with innovation strategy, process optimization, and product development across global markets. 
-                        Whether you're scaling a new technology or refining an existing one, we help bridge the gaps between engineering, manufacturing, and market success.
+                        <p>
+                            We are a semiconductor consulting firm with deep roots in materials science, process engineering, and technology development. 
+                            With over 30 years of experience across capital equipment OEMs, device manufacturers, and consumable suppliers, we understand how all elements of the ecosystem interact—and why that matters. 
+                            Our team brings a unique blend of technical depth and business insight, supporting clients with innovation strategy, process optimization, and product development across global markets. 
+                            Whether you&#39;re scaling a new technology or refining an existing one, we help bridge the gaps between engineering, manufacturing, and market success.
+                        </p>
                         <Image src="/logo_black.png" alt="LycheeByte Logo" width={100} height={100} className="w-[50px] h-auto mt-4 ml-auto pr-2" />
                     </div>
 
